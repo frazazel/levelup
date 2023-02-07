29e9fde341612cb5602f8e9d94284f3501f26256
frazazel <sketchysolid@gmail.com>
2023-02-07 12:58:45 -0600

    prune for release

--- src/tasks/structure.ts
+++
@@ -1,9 +0,0 @@
-import { Quest as BaseQuest, Task as BaseTask, Limit } from "grimoire-kolmafia";
-
-export type Task = BaseTask & {
-  tracking?: string;
-  limit?: Limit;
-  clear?: "all" | "outfit" | "macro" | ("outfit" | "macro")[];
-};
-
-export type Quest = BaseQuest<Task>;
--- upgrade-all.bat
+++
@@ -1,1 +0,0 @@
-yarn upgrade libram@latest kolmafia@latest grimoire-kolmafia@latest eslint-plugin-libram@latest prettier@latest
