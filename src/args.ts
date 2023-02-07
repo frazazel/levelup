f8ea187402bfc94b2843c9b51845b380edd3229c
frazazel <sketchysolid@gmail.com>
2023-02-07 14:11:26 -0600

    group args by type

--- src/args.ts
+++ src/args.ts
@@ -4,19 +4,9 @@ export const args = Args.create(
   "levelup",
   `Written by frazazel (ign: SketchySolid #422389). This is a script that levels you up using inexpensive resources.`,
   {
+    //flags that cause levelup to print info and then abort
     version: Args.flag({
       help: "Output script version number and exit.",
-      default: false,
-      setting: "",
-    }),
-    actions: Args.number({
-      help: "Maximum number of actions to perform, if given. Can be used to execute just a few steps at a time.",
-    }),
-    abort: Args.string({
-      help: "If given, abort during the prepare() step for the task with matching name.",
-    }),
-    sim: Args.flag({
-      help: "If set, see the recommended items and skills, then return without taking any actions.",
       default: false,
       setting: "",
     }),
@@ -24,20 +14,32 @@ export const args = Args.create(
       help: "Show the status of all tasks and exit.",
       setting: "",
     }),
+    sim: Args.flag({
+      help: "If set, see the recommended items and skills, then return without taking any actions.",
+      default: false,
+      setting: "",
+    }),
 
+    //options for partial levelup run
+    actions: Args.number({
+      help: "Maximum number of actions to perform, if given. Can be used to execute just a few steps at a time.",
+    }),
+    abort: Args.string({
+      help: "If given, abort during the prepare() step for the task with matching name.",
+    }),
+
+    //options for configuring how some levelup tasks run
     clan: Args.string({
       help: `Your VIP Clan. Levelup will whitelist into it at the beginning of your day. Requires clan whitelist.`,
     }),
     targetlevel: Args.number({
-      help: `What level to target via adventuring in Uncle Gator's after breaking the prism`,
+      help: `What level to target via adventuring in Uncle Gator's`,
       default: 13,
     }),
-
     buffy: Args.boolean({
       help: "Set this to false to stop asking Buffy for buffs.",
       default: true,
     }),
-
     tip: Args.flag({
       help: "Send all your soap knives to the author. Thanks!",
       default: false,
