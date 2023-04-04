56d409ba3fe8d960224ee12166c4367723a3fad9
frazazel <sketchysolid@gmail.com>
2023-04-04 11:18:34 -0500

    v0.1.5
    - fix for no strange leaflet / no Frobozz house

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ProfitTrackingEngine } from "./engine/engine";
 import { checkReqs } from "./tasks/sim";
 import { args } from "./args";
 
-const version = "0.1.4";
+const version = "0.1.5";
 
 export function main(command?: string): void {
   Args.fill(args, command);
